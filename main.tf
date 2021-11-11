10af36402d41f438661ba35c9cddf3c38a5a4fef
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2021-11-11 09:57:03 -0700

    update for something I have perms for
    Signed-off-by: Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,21 +1,25 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "3.64.2"
     }
   }
 }
 
 variable "name" {
-  type=string
+  type = string
+}
+
+variable "key" {
+  type = string
 }
 
 variable "path" {
-  type=string
+  type = string
 }
 
-resource "aws_iam_group" "this_group" {
-  name = var.name
-  path = var.path
+resource "aws_key_pair" "these_keys" {
+  name       = var.name
+  public_key = var.key
 }
